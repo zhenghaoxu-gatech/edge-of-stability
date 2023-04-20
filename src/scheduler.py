88c094af524443a413c61f078fae572c8ac0bcee
zhenghaoxu-gatech <zhenghaoxu@gatech.edu>
2023-04-20 15:11:08 -0400

    -

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,8 +1,6 @@
 import os
 import numpy as np
 
-np.random.bit_generator = np.random._bit_generator
-
 for lr in np.arange(0.1, 0.2, 0.02):
     os.system(f"python src/gd.py cifar10-5k fc-relu-depth1  mse {lr} 5000 --acc_goal 0.99 --neigs 2  --eig_freq 10")
     os.system(f"python src/gd.py cifar10-5k fc-relu-depth1  ce {lr} 5000 --acc_goal 0.99 --neigs 2  --eig_freq 10")
