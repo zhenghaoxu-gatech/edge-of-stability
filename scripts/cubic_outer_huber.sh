bf0ae86c04634185c89cfe3789178bc3bdfab645
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-03 00:58:26 -0400

    bash

--- scripts/cubic_outer_huber.sh
+++ scripts/cubic_outer_huber.sh
@@ -4,6 +4,6 @@ for weight in "w1_3_w2_10"
 do
     for lr in 0.000016
     do
-        python src/gd.py cifar10-1k fc-cubic_relu-outer  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight {weight}
+        python src/gd.py cifar10-1k fc-cubic_relu-outer  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
 done
