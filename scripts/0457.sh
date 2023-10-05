5f8a29cfe2342bf2762ea2aa5bce31fe881184a5
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-05 00:33:35 -0400

    0457 cubic_relu+mse+BN seed 1 for positive initial

--- scripts/0457.sh
+++ scripts/0457.sh
@@ -4,6 +4,6 @@ for weight in "w1_6_w2_20"
 do
     for lr in 0.00002 0.00006 0.0001 0.00014 0.00018
     do
-        python src/gd.py cifar10-1k fc-cubic_relu-depth1  mse $lr 100000 --batch_norm --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py cifar10-1k fc-cubic_relu-depth1  mse $lr 100000 --batch_norm --neigs 1 --seed 1  --eig_freq 400 --width 200 --init_weight $weight
     done
 done
