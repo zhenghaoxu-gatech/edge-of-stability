16c538960fc6bc8745bde08d7b0612995ebb15b3
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-07 03:45:01 -0400

    mnist shorter

--- scripts/0457_mnist.sh
+++ scripts/0457_mnist.sh
@@ -2,20 +2,20 @@
 
 for weight in "w1_10_w2_40"
 do
-    for lr in 0.0001 0.0002 0.0003 0.0004 0.0005
+    for lr in 0.0002 0.0003 0.0004 0.0005
     do
-        python src/gd.py mnist fc-relu-depth1  mse $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-relu-depth1  mse $lr 100000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
 
     for lr in 0.0001 0.00012 0.00014 0.00016 0.00018
     do
-        python src/gd.py mnist fc-cubic_relu-depth1  mse $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-cubic_relu-depth1  mse $lr 100000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
 
     for lr in 0.001 0.003 0.005 0.007 0.009
     do
-        python src/gd.py mnist fc-tanh-depth1  mse $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
-        python src/gd.py mnist fc-relu-depth1  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-tanh-depth1  mse $lr 100000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-relu-depth1  huber $lr 100000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
     
 done
--- scripts/0458_mnist.sh
+++ scripts/0458_mnist.sh
@@ -2,14 +2,14 @@
 
 for weight in "w1_10_w2_40"
 do
-    for lr in 0.001 0.003 0.005 0.007 0.009
+    for lr in 0.003 0.005 0.007 0.009
     do
-        python src/gd.py mnist fc-tanh-depth1  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-tanh-depth1  huber $lr 100000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
 
     for lr in 0.007 0.01 0.015 0.02 0.025 0.03
     do
-        python src/gd.py mnist fc-relu-depth1  mse $lr 200000 --batch_norm --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-relu-depth1  mse $lr 100000 --batch_norm --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
     
 done
@@ -18,7 +18,7 @@ for weight in "w1_8_w2_20"
 do
     for lr in 0.0001 0.0002 0.0003 0.0004 0.0005
     do
-        python src/gd.py mnist fc-cubic_relu-outer  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-cubic_relu-outer  huber $lr 100000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
 
     
