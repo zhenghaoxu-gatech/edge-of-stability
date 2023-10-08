783f87d9828a0fd20762ad73d7808a5cbb9ca03d
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-08 00:05:37 -0400

    test cubic_outer_mnist

--- scripts/0458_mnist.sh
+++ scripts/0458_mnist.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-for weight in "w1_10_w2_40"
-do
+# for weight in "w1_10_w2_40"
+# do
 
-    for lr in 0.04 0.05 0.07 0.09 0.12
-    do
-        python src/gd.py mnist fc-relu-depth1  mse $lr 10000 --batch_norm --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
-    done
+#     for lr in 0.04 0.05 0.07 0.09 0.12
+#     do
+#         python src/gd.py mnist fc-relu-depth1  mse $lr 10000 --batch_norm --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+#     done
     
-done
+# done
 
 for weight in "w1_6_w2_15"
 do
-    for lr in 0.0001 0.0002 0.0003 0.0004 0.0005
+    for lr in 0.00001 0.00002 0.00003 0.00004 0.00005
     do
         python src/gd.py mnist fc-cubic_relu-outer  huber $lr 10000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
