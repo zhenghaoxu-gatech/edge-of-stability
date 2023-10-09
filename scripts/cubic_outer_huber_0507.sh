8578779221033fda3bd0c440b1b619a48ea34dd5
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-09 09:53:34 -0400

    cubic outer

---
+++ scripts/cubic_outer_huber_0507.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for weight in "w1_3_w2_10"
+do
+    for lr in 0.000012
+    do
+        python src/gd.py cifar10-1k fc-cubic_relu-outer  huber $lr 2000000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+    done
+done
