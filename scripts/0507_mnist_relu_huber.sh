81fea56ffaeaef722d83ef81b987d4a208565d23
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-10 18:16:16 -0400

    mnist longer

---
+++ scripts/0507_mnist_relu_huber.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for weight in "w1_10_w2_40"
+do
+    for lr in 0.001 0.003 0.005 0.007 0.009
+    do
+        python src/gd.py mnist fc-relu-depth1  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+    done
+done
---
+++ scripts/0531_mnist_cubic_relu_mse_BN.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for weight in "w1_10_w2_40"
+do
+    for lr in 0.001 0.002 0.003 0.004 0.005
+    do
+        python src/gd.py mnist fc-cubic_relu-depth1  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+    done
+done
