045a4f986023d767a1aea7dbd89ed867461a182a
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-11 02:30:08 -0400

    longer mnist

--- scripts/0307_mnist_cubic_relu_huber.sh
+++ scripts/0307_mnist_cubic_relu_huber.sh
@@ -4,6 +4,6 @@ for weight in "w1_10_w2_40"
 do
     for lr in 0.001 0.002 0.003 0.004 0.005
     do
-        python src/gd.py mnist fc-cubic_relu-depth1  huber $lr 200000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+        python src/gd.py mnist fc-cubic_relu-depth1  huber $lr 500000 --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
     done
 done
