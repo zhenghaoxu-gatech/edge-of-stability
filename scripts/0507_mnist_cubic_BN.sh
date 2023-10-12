3da49072a6c8074e394c6609404cd22bc25d0cb9
Zhenghao Xu <zhenghaoxu@gatech.edu>
2023-10-11 22:33:20 -0400

    more BN

---
+++ scripts/0507_mnist_cubic_BN.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for weight in "w1_6_w2_20"
+do
+    for lr in 0.001 0.002 0.003 0.004 0.005
+    do
+        python src/gd.py mnist fc-cubic_relu-depth1  huber $lr 1000000 --batch_norm --neigs 1  --eig_freq 400 --width 200 --init_weight $weight
+    done
+done
